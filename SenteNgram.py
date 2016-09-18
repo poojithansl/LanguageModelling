f5b3fb8d95c9df6e38d2d9ef7e57877f29a1532c
sa7i <poojithansl007@gmail.com>
2016-09-18 12:05:13 +0530

    Updating Ngram gen

--- SenteNgram.py
+++ SenteNgram.py
@@ -32,61 +32,36 @@ t_re=re.compile((mixtheregex(email,url,word,quotes,number,hashi,mention,ellipsis
 jm=[]
 phi=0
 def Tokenize(s):
-	"""
-	if SentenceT(s):
-		print "a"
-	"""
-	#print s
-	#print "-------"
-	
-	
-	#print "============"
+
 	s=str(s).decode('utf-8')
-	#print s
+	
 	tokens=t_re.findall(s)
-	#print tokens
-	#print tokens[-1].encode('utf-8')	
-	#print tokens
-	#print '-====-'
-	"""
+	
 	if tokens!=[]:
-		print tokens[-3:-1]
-	"""
-	if tokens!=[]:
-		#print tokens
+		
 		jm=[]
 		for k in range(len(tokens)):
 			for j in range(k,k+phi):
 				if k+phi<len(tokens):
 					jm.append(tokens[j].encode('utf-8'))
-			#print k
-			#print "kkkkkkkk"
-		#print jm
-		#print j
+			
 			m=" ".join(jm)
 			if not jer.has_key(m):
 				jer[m]=1
 			else:
 				jer[m]+=1
 			jm=[]	
-		#print jer
-		#print "===jer in tokenise=="	
-	#for k in tokens:	
-	#	print k.encode('utf-8')
-		#print tokens.encode('utf-8')
+		
 	return tokens
-	#print tokens
+	
 	
 if __name__=="__main__":
-	#f=codecs.open('test.txt',encoding='utf-8')
-	#for j in f:
+	
 	phi=1
 	mp=[]
-	pooj=input("Enter n as a limit to ngram")
+	po=input("Enter n as a limit to ngram")
 	for j in open('k2.txt', mode='r').readlines():
-			#with open('test.txt',encoding='utf-8') as f:
-			#print j
-			#print "--------"
+			
 			mp.append(j)
 			l=Tokenize(j)
 	#print jer
@@ -99,24 +74,21 @@ if __name__=="__main__":
 		if jer[k]>ml and k!='':
 			ml=jer[k]
 			qe=k
-			#print ml
+			
 	print qe
 	onk=''
 	print "***"
 	for jmk in range(30):			#30 iterations to produce a paragraph
 		
-		for phi in range(2,pooj+1):			#bigrams to n-grams
+		for phi in range(2,po+1):			#bigrams to n-grams
 		
 			jer={}
 			ml=-99
 			t=[]
 			for j in mp:
-					#with open('test.txt',encoding='utf-8') as f:
-					#print j
-					#print "--------"
+					
 					l=Tokenize(j)
-					#print j
-					#print jer
+					
 			flag=0
 			for k in jer.keys():
 				#t=k.split()
@@ -139,5 +111,6 @@ if __name__=="__main__":
 		onk=onk+qe
 		qe="".join(mko[-1])
 		"""
+		
 	print "=====Sentences Generated======"	
 	print onk
